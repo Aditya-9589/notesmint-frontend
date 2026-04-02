0ebc8b4bea206d731a950b2a1f79cc6e7ad642a8
Aditya Hariharno <work.adityah@gmail.com>
2026-04-02 17:07:49 +0530

    frontend structure ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+// import BASE_URL from "../config/api";
+
+// fetch(`${BASE_URL}/payment/create-order`, {
+//   method: "POST",
+// });
 import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1 className="text-4xl text-green-600 font-bold">
+        Tailwind Working 🚀
+      </h1>
+    </div>
   )
 }
 
